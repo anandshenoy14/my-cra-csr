7f2114e04b6c30d26b69eba82afa5043f848bcb0
Anand <anand.shenoy14@gmail.com>
2019-07-13 22:43:20 -0700

    adding search capability in chart

--- src/chart/Chart.js
+++ src/chart/Chart.js
@@ -31,7 +31,12 @@ class Chart extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            employees : []
+            original : {
+                employees : []
+            },
+            current : {
+                employees : []
+            }
         }
     }
     componentDidMount(){
@@ -39,21 +44,70 @@ class Chart extends React.Component{
         .then(res => res.json())
         .then(
             (result) => {
+            let original = this.state.original.employees.slice();
+            let current = this.state.current.employees.slice();
+            original = result;
+            current = result;
             this.setState({
-                employees: result
+                original : {
+                    employees : original
+                },
+                current : {
+                    employees : current
+                }
             });
             },
             (error) => {
             this.setState({
+               "current" : {
                 employees : [{
                     "name" : "An Error Occured While Fetching Chart"
                 }]
+               }
             });
             }
         )
     }
+    findQuery(employees,name, result){
+        employees.forEach((employee)=>{
+            if(employee.employees && employee.employees.length > 0){
+                if(employee.name.indexOf(name) > -1){
+                    result.push(employee)
+                }
+                result = this.findQuery(employee.employees,name,result)
+            }else{
+                if(employee.name.indexOf(name) > -1){
+                    result.push(employee)
+                }
+            }
+        })
+        return result;
+    }
+    searchAndUpdate(e){
+        let inputSearchTerm = (e.target) && (e.target.value);
+        let employees = this.state.original.employees;
+        let foundEmployeesWithName = this.findQuery(employees,inputSearchTerm,[]);
+        if(inputSearchTerm != ""){
+            this.setState({
+                "current" : {
+                    "employees" : foundEmployeesWithName
+                }
+            })
+        }else{
+            this.setState({
+                "current" : {
+                    "employees" : employees
+                }
+            })
+        }
+    }
     render(){
-        return (<EmployeeList employees={this.state.employees} ceo="y"></EmployeeList>)
+        return (
+            <div>
+                <input type="text" onChange={this.searchAndUpdate.bind(this)} value={this.props.children}></input>
+                <div><EmployeeList employees={this.state.current.employees} ceo="y"></EmployeeList></div>
+            </div>
+        )
     }
 }
 
